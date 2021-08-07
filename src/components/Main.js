7c071f30f041cbebb2c6483442d7d8945d2fb900
abdheshnayak <abdheshnayak@gmail.com>
2021-08-07 10:09:50 +0545

    :tada: init

---
+++ src/components/Main.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import React, { createRef, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "./main.scss";
+
+function Main() {
+  const [image, setimage] = useState("");
+  const [is_loading, setis_loading] = useState(false);
+
+  useEffect(() => {
+    captureUrl();
+    // axios("https://api.anayak.com.np/url-capture/?url=https://progman.in").then(
+    //   (res) => {
+    //     setimage(res.data);
+    //   }
+    // );
+  }, []);
+
+  const my_ref = createRef();
+
+  const validURL = (str) => {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+    return !!pattern.test(str);
+  };
+
+  const captureUrl = () => {
+    if (!my_ref.current.value) {
+      toast.error("Url is not provided.");
+      return;
+    } else if (!validURL(my_ref.current.value)) {
+      toast.error("This is not a valid url.");
+      return;
+    } else if (my_ref.current.value.substring(0, 4) != "http") {
+      toast.error("URL must start with http");
+      return;
+    }
+    setis_loading(true);
+    axios({
+      url: "https://api.anayak.com.np/url-capture/",
+      params: { url: my_ref.current.value },
+    })
+      .then((res) => {
+        setis_loading(false);
+        setimage(res.data);
+      })
+      .catch((err) => {
+        setis_loading(false);
+      });
+  };
+
+  return (
+    <div className="main">
+      <div className="app-bar">
+        <div className="container">
+          <h2>
+            URL CAPTURE <span style={{ color: "#789ABC" }}>|</span>{" "}
+            <span style={{ color: "#aaa" }}>ANAYAK</span>
+          </h2>
+        </div>
+      </div>
+      <div style={{ padding: "1.75rem" }}></div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          captureUrl();
+        }}
+      >
+        <div className="button-box-cont">
+          <input
+            type="text"
+            ref={my_ref}
+            placeholder="https://facebook.com"
+            defaultValue="https://progman.in"
+          />
+          <input type="submit" value="Capture" />
+        </div>
+      </form>
+
+      <div className="container">
+        <div className="image-container">
+          <img src={image} alt="" />
+          {is_loading && <div className="spinner"></div>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
