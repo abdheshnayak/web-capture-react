bcc9d167f00884ae624f514423921e0bdb413893
Abdhesh Nayak <abdheshnayak@gmail.com>
2023-03-07 17:34:46 +0530

    :construction: wip

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,13 @@ import Footer from '@commons/components/footer';
 import Header from '@commons/components/header';
 // import '../styles/globals.scss';
 import '../styles/main.scss';
+import { ToastContainer  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="min-w-min min-h-screen flex flex-col">
+      <ToastContainer />
       <Header />
       <div className="flex-1 py-4 flex flex-col">
         <Component {...pageProps} />
